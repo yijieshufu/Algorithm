a347138dc0ce7b1b09dab5d56fbd4f9073730fd6
YU310-WINER <zhaoyu4@stu.cdut.edu.cn>
2026-03-10 10:52:57 +0800

    vault backup: 2026-03-10 10:52:57

--- 算法学习/二分解题法/倒水-真题.py
+++ 算法学习/二分解题法/倒水-真题.py
@@ -1,12 +1,25 @@
 import sys
-
 # 数据读入
 it = iter(sys.stdin.read().split())
-
 n = int(next(it))
 k = int(next(it))
 a = [int(next(it)) for _ in range(n)]
-
 # check h函数
 def check(mid):
-    for i in range
+    for state in range(k):
+        surplus = 0
+        for i in range(state,n,k):
+            surplus += a[i]-mid
+            if surplus<0:
+                return False
+    return True
+l,r=0,10**14
+ans = 0
+while l<=r:
+    mid = (l+r)//2
+    if check(mid):
+        ans = mid 
+        l = mid+1
+    else:
+        r = mid-1
+print(ans)
